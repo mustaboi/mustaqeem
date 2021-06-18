a3cefd84d7d6c0d76fe17865c7cab3bd3529db68
Bilalzaghum <191108@students.au.edu.pk>
2021-06-18 23:50:47 +0500

    first commit

--- app/src/main/java/com/example/fitnessapp/MainActivity.java
+++ app/src/main/java/com/example/fitnessapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.fitnessapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void onclick_workout(View view){
+        Intent intent = new Intent(this, workout.class);
+        startActivity(intent);
+    }
+    public void onclick_diet(View view){
+        Intent intent = new Intent(this, Diet.class);
+        startActivity(intent);
+    }
+    public void onclick_routine(View view){
+        Intent intent = new Intent(this, workout.class);
+        startActivity(intent);
+    }
+    public void onclick_reminder(View view){
+        Intent intent = new Intent(this, workout.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/fitnessapp/exercise.java
+++ app/src/main/java/com/example/fitnessapp/exercise.java
@@ -1,4 +1,36 @@
 package com.example.fitnessapp;
 
 public class exercise {
+
+    String level,ex_name,equipments;
+
+    public exercise(String level, String ex_name, String equipments) {
+        this.level = level;
+        this.ex_name = ex_name;
+        this.equipments = equipments;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getEx_name() {
+        return ex_name;
+    }
+
+    public void setEx_name(String ex_name) {
+        this.ex_name = ex_name;
+    }
+
+    public String getEquipments() {
+        return equipments;
+    }
+
+    public void setEquipments(String equipments) {
+        this.equipments = equipments;
+    }
 }
